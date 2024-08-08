59d09869bf5351a924d54e1388a10f31af0b9a76
Jared Watts <jbw976@gmail.com>
2024-08-08 07:34:59 -0700

    attempt at client side streaming
    Signed-off-by: Jared Watts <jbw976@gmail.com>

--- server/server.go
+++ server/server.go
@@ -1,9 +1,9 @@
 package server
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"time"
 
 	"google.golang.org/grpc/codes"
@@ -18,41 +18,56 @@ type Server struct {
 	changelogs.UnimplementedChangeLogServiceServer
 }
 
-func (s *Server) SendChangeLog(ctx context.Context, req *changelogs.SendChangeLogRequest) (*changelogs.SendChangeLogResponse, error) {
-	if req == nil || req.Entry == nil {
-		st := status.New(codes.Internal, "Request and change logs entry must not be nil")
-		return &changelogs.SendChangeLogResponse{}, st.Err()
+func (s *Server) SendChangeLog(stream changelogs.ChangeLogService_SendChangeLogServer) error {
+	callCount := 0
+	for {
+		callCount++
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&changelogs.SendChangeLogResponse{})
+		}
+		if err != nil {
+			return err
+		}
+
+		if req != nil {
+			st := status.New(codes.Internal, fmt.Sprintf("fake error %d from jared", callCount))
+			return st.Err()
+		}
+
+		if req == nil || req.Entry == nil {
+			st := status.New(codes.Internal, "Request and change logs entry must not be nil")
+			return st.Err()
+		}
+
+		// Marshal the change log entry coming over the wire to JSON using the
+		// protojson helper
+		b, err := protojson.Marshal(req.Entry)
+		if err != nil {
+			st := status.New(codes.Internal, errors.Wrap(err, "Failed to marshall input entry").Error())
+			return st.Err()
+		}
+
+		// Now unmarshal those bytes into a map so we can inject fields/data that
+		// are server side only
+		var entryData map[string]interface{}
+		if err := json.Unmarshal(b, &entryData); err != nil {
+			st := status.New(codes.Internal, errors.Wrap(err, "Failed to unmarshal input entry").Error())
+			return st.Err()
+		}
+
+		// Include a server side current timestamp into the data
+		entryData["timestamp"] = time.Now().UTC().Format(time.RFC3339)
+
+		// Marshal the data back to JSON bytes once again so we can write the final
+		// product to the output
+		b, err = json.Marshal(entryData)
+		if err != nil {
+			st := status.New(codes.Internal, errors.Wrap(err, "Failed to marshal final entry").Error())
+			return st.Err()
+		}
+
+		// write the final change log entry to stdout
+		fmt.Printf("%s\n", string(b))
 	}
-
-	// Marshal the change log entry coming over the wire to JSON using the
-	// protojson helper
-	b, err := protojson.Marshal(req.Entry)
-	if err != nil {
-		st := status.New(codes.Internal, errors.Wrap(err, "Failed to marshall input entry").Error())
-		return &changelogs.SendChangeLogResponse{}, st.Err()
-	}
-
-	// Now unmarshal those bytes into a map so we can inject fields/data that
-	// are server side only
-	var entryData map[string]interface{}
-	if err := json.Unmarshal(b, &entryData); err != nil {
-		st := status.New(codes.Internal, errors.Wrap(err, "Failed to unmarshal input entry").Error())
-		return &changelogs.SendChangeLogResponse{}, st.Err()
-	}
-
-	// Include a server side current timestamp into the data
-	entryData["timestamp"] = time.Now().UTC().Format(time.RFC3339)
-
-	// Marshal the data back to JSON bytes once again so we can write the final
-	// product to the output
-	b, err = json.Marshal(entryData)
-	if err != nil {
-		st := status.New(codes.Internal, errors.Wrap(err, "Failed to marshal final entry").Error())
-		return &changelogs.SendChangeLogResponse{}, st.Err()
-	}
-
-	// write the final change log entry to stdout
-	fmt.Printf("%s\n", string(b))
-
-	return &changelogs.SendChangeLogResponse{}, nil
 }
